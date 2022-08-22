da7c0bb7ab893d2bc6bea13660d38a4deba7210c
Abhishek Kumar Yadav <abhishekyadavjan18@gmail.com>
2022-08-22 13:51:36 +0530

    code updated

---
+++ index.js
@@ -0,0 +1,28 @@
+const http = require("http");
+const express= require("express");
+const socketIO = require("socket.io")
+const cors = require("cors");
+
+const app = express();
+const port = 4500;
+
+const server = http.createServer(app);
+const io = socketIO(server);
+app.use(cors());
+
+app.use("/", (req, res)=>{
+    res.send('hello world')
+})
+
+io.on('connection',()=>{
+    console.log('someone connected')
+})
+
+
+
+server.listen(port,()=>{
+    console.log(`express server running on port ${port}`)
+})
+
+
+
