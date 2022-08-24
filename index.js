254c4fe61b51474c9e0af1a686336f9246690c01
Abhishek Kumar Yadav <abhishekyadavjan18@gmail.com>
2022-08-24 17:43:49 +0530

    server code updated

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const http = require("http");
-const express= require("express");
+const express = require("express");
 const socketIO = require("socket.io")
 const cors = require("cors");
 
@@ -10,17 +10,34 @@ const server = http.createServer(app);
 const io = socketIO(server);
 app.use(cors());
 
-app.use("/", (req, res)=>{
+const users = [{}];
+console.log(users);
+app.use("/", (req, res) => {
     res.send('hello world')
 })
 
-io.on('connection',()=>{
-    console.log('someone connected')
+io.on('connection', (socket) => {
+    // console.log("hello connected");
+    socket.on("joined", ({ user }) => {
+        users[socket.id] = user;
+        socket.emit('welcome', { user: "admin", message: `Welcome to the Chat:${users[socket.id]}` })
+        socket.broadcast.emit('userJoined', { user: "admin", message: `${users[socket.id]} has joined` })
+    })
+    socket.on("message",({chat,id})=>{
+        io.emit('sendMessage',{user:users[id],message:chat,id})
+
+    })
+    socket.on('disconnect', () => {
+        socket.broadcast.emit("leave", { user: "Admin", message: `${users[socket.id]},has left` })
+
+        console.log("user has left");
+    })
+
 })
- 
 
 
-server.listen(port,()=>{
+
+server.listen(port, () => {
     console.log(`express server running on port ${port}`)
 })
 
